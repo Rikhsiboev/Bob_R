9bd241bd362ca4f46919cd3f3bdaa37354a58dfb
Rikhsiboev <rikhsiboev@gmail.com>
2023-01-29 16:22:16 -0700

    SumAllNumbersAsString Still Learning

--- src/main/java/Practice_Code/SumAllNumbersAsString.java
+++ src/main/java/Practice_Code/SumAllNumbersAsString.java
@@ -4,6 +4,17 @@ import java.util.function.Function;
 
 public class SumAllNumbersAsString {
     public static void main(String[] args) {
+        /// 1) Interface Function public interface Function<T, R>
+        /// 2) Lambda
+        /// 3) For each loop for String to find
+        ///    a) trim remove all space text from front and back
+        ///    b) Split separate all " " space to get as array
+        ///    c) to find each Char of String as Index For loop
+        ///    d) Character.isDigit  inbuilt method with String method chartAt
+        ///    e) if from String we get digit, and we're comparing with String by ?  Integer.valueOf(s) : 0
+
+
+
         // interface with 2 <T,I> name              lambda
         Function<String, Integer> sumOfAllIntegers = p -> {
             // for get final result
@@ -18,7 +29,7 @@ public class SumAllNumbersAsString {
                                             // finding digit in String
                     count += (Character.isDigit(s.charAt(i))) ? 1 : 0;
                 }
-
+                                            // wrapped class with valueOf method
                 sum += (count == s.length()) ? Integer.valueOf(s) : 0;
             }
             return sum;
