8aa2db4528992c3a3339f5491b38d6826bb6018a
Rikhsiboev <rikhsiboev@gmail.com>
2023-02-22 20:07:49 -0700

    Bob_R    /// WHEN WE CREATE CONSTRUCTOR IT WILL BE AUTOMATIC    @Autowired

--- spring-05-Core-Abstraction/src/main/java/Bob_R/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/Bob_R/service/CommentService.java
@@ -3,15 +3,22 @@ package Bob_R.service;
 import Bob_R.model.Comment;
 import Bob_R.proxy.CommentNotificationProxy;
 import Bob_R.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
+   // @Autowired
     private CommentRepository commentRepository;
     private CommentNotificationProxy commentNotificationProxy;
-
+   // @Autowired
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
-
-
+   /// WHEN WE CREATE CONSTRUCTOR IT WILL BE AUTOMATIC    @Autowired
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 }
