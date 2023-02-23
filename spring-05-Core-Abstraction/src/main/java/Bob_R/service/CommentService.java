bbaae134ad7c51dad45dfa8d20ddd06ec596addb
Rikhsiboev <rikhsiboev@gmail.com>
2023-02-22 20:31:14 -0700

    ,@Qualifier("EmailCommentNotificationProxy")

--- spring-05-Core-Abstraction/src/main/java/Bob_R/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/Bob_R/service/CommentService.java
@@ -4,20 +4,22 @@ import Bob_R.model.Comment;
 import Bob_R.proxy.CommentNotificationProxy;
 import Bob_R.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentService {
    // @Autowired
-    private CommentRepository commentRepository;
-    private CommentNotificationProxy commentNotificationProxy;
+    /// don`t forget to write final make sure that constructor correct adn added
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
    // @Autowired
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
    /// WHEN WE CREATE CONSTRUCTOR IT WILL BE AUTOMATIC    @Autowired
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository,@Qualifier("EmailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
