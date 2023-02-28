9096d0e637ce646d4c6cf9c39ee70fe206117da0
Rikhsiboev <rikhsiboev@gmail.com>
2023-02-28 09:53:54 -0700

    FIX

---
+++ SPRING-08-MVC-CONTROLLER/src/main/java/com/bob/BOB_R_Application.java
@@ -0,0 +1,13 @@
+package com.bob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BOB_R_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BOB_R_Application.class, args);
+    }
+
+}
