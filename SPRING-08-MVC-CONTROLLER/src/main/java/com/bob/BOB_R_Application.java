59382d371aa1da9e1bb3af7a8841ac64891991cb
Rikhsiboev <rikhsiboev@gmail.com>
2023-02-28 11:36:22 -0700

    MVC

--- SPRING-08-MVC-CONTROLLER/src/main/java/com/bob/BOB_R_Application.java
+++ SPRING-08-MVC-CONTROLLER/src/main/java/com/bob/BOB_R_Application.java
@@ -2,8 +2,10 @@ package com.bob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {AopAutoConfiguration.class})
 public class BOB_R_Application {
 
     public static void main(String[] args) {
