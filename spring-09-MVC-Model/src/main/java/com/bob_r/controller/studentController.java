5683b817efd90ed809fa27f0e967d191b2e70a0a
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-02 11:09:46 -0700

    MVC-model

--- spring-09-MVC-Model/src/main/java/com/bob_r/controller/studentController.java
+++ spring-09-MVC-Model/src/main/java/com/bob_r/controller/studentController.java
@@ -3,6 +3,11 @@ package com.bob_r.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class studentController {
     @RequestMapping("/welcome")
@@ -10,6 +15,16 @@ public class studentController {
         // model interface
         model.addAttribute("name","Course");
         model.addAttribute("course","Abc");
+        String subject = "Spring Boot";
+        model.addAttribute("subject",subject);
+        int studentId = new Random().nextInt();
+        model.addAttribute("id",studentId);
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        model.addAttribute("numbers",numbers);
         return "/student/welcome";
     }
 }
