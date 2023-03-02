d0ff94eaa44e3e78934eeebf4d623337a40522da
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-02 11:23:18 -0700

    MVC-model

--- spring-09-MVC-Model/src/main/java/com/bob_r/controller/studentController.java
+++ spring-09-MVC-Model/src/main/java/com/bob_r/controller/studentController.java
@@ -1,12 +1,15 @@
 package com.bob_r.controller;
 
+import com.bob_r.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 @Controller
 public class studentController {
@@ -25,6 +28,12 @@ public class studentController {
         numbers.add(6);
         numbers.add(7);
         model.addAttribute("numbers",numbers);
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date",dt);
+
+        Student student = new Student(UUID.randomUUID(),"Mike","Smith");
+        model.addAttribute("student",student);
+
         return "/student/welcome";
     }
 }
