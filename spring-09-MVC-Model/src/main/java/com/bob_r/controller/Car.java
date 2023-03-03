d1a884eb5c63532bc962ca989798fd988bb0808e
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-02 22:15:34 -0700

    .

--- spring-09-MVC-Model/src/main/java/com/bob_r/controller/Car.java
+++ spring-09-MVC-Model/src/main/java/com/bob_r/controller/Car.java
@@ -2,6 +2,7 @@ package com.bob_r.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.model.IModel;
@@ -40,4 +41,16 @@ public class Car {
 
         return "/car/car-info";
     }
+
+
+    //localhost:8080/info/honda/2015
+    @RequestMapping("/info/{make}/{year}")
+    public String getCarInfo2(@PathVariable ("make")String car,@PathVariable String year){
+
+        System.out.println(car);
+        System.out.println(year);
+       // honda
+       return "car/car-info";
+    }
+
 }
