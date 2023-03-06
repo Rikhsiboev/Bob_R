5b8decc2d568b30984e43fe914cbb6b480587667
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-06 15:43:11 -0700

    .

--- spring-10-Thymeleaf/src/main/java/com/bob_r/controller/mentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/bob_r/controller/mentorController.java
@@ -1,17 +1,33 @@
 package com.bob_r.controller;
 
+import com.bob_r.nodel.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/mentor")
 public class mentorController {
 
-    @RequestMapping("/register") //localhost8080/register
-    public String register(){
-        return "student/register";
+
+    @GetMapping("/register") //localhost8080/register
+    public String register(Model model){
+        model.addAttribute("mentor",new Mentor());
+
+        return "mentor/mentor-register";
     }
-    @RequestMapping("/drop") //localhost8080/mentor/drop
-    public String drop(){
-        return "mentor/register";
-    }
+
+
+//    @RequestMapping("/register") //localhost8080/register
+//    public String register(){
+//        return "student/register";
+//    }
+
+
+
+
+//    @RequestMapping("/drop") //localhost8080/mentor/drop
+//    public String drop(){
+//        return "mentor/register";
+//    }
 }
