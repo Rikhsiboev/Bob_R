1990f72231f7135805adaf377744e7eff75e258f
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-07 10:34:03 -0700

    .

--- spring-10-Thymeleaf/src/main/java/com/bob_r/controller/mentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/bob_r/controller/mentorController.java
@@ -4,17 +4,33 @@ import com.bob_r.nodel.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class mentorController {
 
 
     @GetMapping("/register") //localhost8080/register
-    public String register(Model model){
-        model.addAttribute("mentor",new Mentor());
+    public String register(Model model) {
+        List<String> batchlist = Arrays.asList("JD1", "JD2", "JD3");
+        model.addAttribute("batchList", batchlist);
+        model.addAttribute("mentor", new Mentor());
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor")Mentor mentor) {
+        // How i cab access to mentor attribute in this method
+
+
+        return "mentor/mentor-confirmation";
     }
 
 
@@ -22,8 +38,6 @@ public class mentorController {
 //    public String register(){
 //        return "student/register";
 //    }
-
-
 
 
 //    @RequestMapping("/drop") //localhost8080/mentor/drop
