8c0d8599097164de97304f12a811b32b5f02a3c6
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-09 19:44:10 -0700

    .

--- Employee_Lab/src/main/java/com/bob_r/controller/EmployeeController.java
+++ Employee_Lab/src/main/java/com/bob_r/controller/EmployeeController.java
@@ -6,10 +6,13 @@ import com.bob_r.service.EmployeeService;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -32,8 +35,16 @@ public class EmployeeController {
         return "employee/employee-create";
     }
 
+
+    // coming from action bottom from HTML
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee) {
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult,Model model) {
+        // passing value with bindingResult with if method
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("stateList", DataGenerator.getAllState());
+            return "employee/employee-create";
+        }
+
         //saving employer
         employeeService.saveEmployee(employee);
         //return as redirect to list @GetMapping("/list")
--- Employee_Lab/src/main/java/com/bob_r/model/Employee.java
+++ Employee_Lab/src/main/java/com/bob_r/model/Employee.java
@@ -5,11 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
+//    @NotNull    //-> field shouldn`t be null
+//    @NotEmpty   //-> "" field shouldn`t empty
+//    @NotBlank   //-> " " field shouldn`t Blank
+
+    @NotBlank  //-> cover all
+    @Size(max = 12 , min = 2)
     private String firstName;
     private String lastName;
     //thymeleaf accept yyyy-mm-dd this format but LocalDate accepts mm-dd-yyyy
