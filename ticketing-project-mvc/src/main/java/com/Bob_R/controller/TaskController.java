c34fe18c580eed5b17e0c20b2352b06509b8eba4
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-16 20:53:14 -0600

    .

--- ticketing-project-mvc/src/main/java/com/Bob_R/controller/TaskController.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/controller/TaskController.java
@@ -1,18 +1,24 @@
 package com.Bob_R.controller;
 
 import com.Bob_R.dto.TaskDTO;
+import com.Bob_R.service.ProjectService;
 import com.Bob_R.service.TaskService;
+import com.Bob_R.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/task")
 public class TaskController {
+
+    private final ProjectService projectService;
+    private final UserService userService;
     private final TaskService taskService;
 
-    public TaskController(TaskService taskService) {
+    public TaskController(ProjectService projectService, UserService userService, TaskService taskService) {
+        this.projectService = projectService;
+        this.userService = userService;
         this.taskService = taskService;
     }
 
@@ -21,10 +27,25 @@ public class TaskController {
     public String taskCreate(Model model) {
 
         model.addAttribute("task", new TaskDTO());
-        model.addAttribute("task", new TaskDTO());
-
-
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
         return "/task/create";
     }
+
+    @PostMapping("/create")
+    public String insertTask(@ModelAttribute("task") TaskDTO task) {
+        taskService.save(task);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id) {
+        taskService.deleteById(id);
+        return "redirect:/task/create";
+    }
+
+
+
 
 }
--- ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/TaskServiceImp.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/TaskServiceImp.java
@@ -1,14 +1,27 @@
 package com.Bob_R.service.impl;
 
 import com.Bob_R.dto.TaskDTO;
+import com.Bob_R.enums.Status;
 import com.Bob_R.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
+
 @Service
 public class TaskServiceImp extends AbstractMapService <TaskDTO,Long> implements TaskService {
     @Override
     public TaskDTO save(TaskDTO task) {
+        if(task.getTaskStatus()==null) {
+            task.setTaskStatus(Status.OPEN);
+        }
+        if (task.getAssignedDate()==null){
+            task.setAssignedDate(LocalDate.now());
+        }
+        if (task.getId()==null){
+            task.setId(UUID.randomUUID().getMostSignificantBits());
+        }
         return super.save(task.getId(),task);
     }
 
