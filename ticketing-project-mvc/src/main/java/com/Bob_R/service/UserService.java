c41d189a5438520c3a102d598dc275bbca5eaf69
Rikhsiboev <rikhsiboev@gmail.com>
2023-03-27 17:24:07 -0600

    .

--- ticketing-project-mvc/src/main/java/com/Bob_R/service/UserService.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/service/UserService.java
@@ -5,6 +5,7 @@ import com.Bob_R.dto.UserDTO;
 import java.util.List;
 
 public interface UserService extends CrudService <UserDTO,String> {
+
     List<UserDTO> findManagers();
     List<UserDTO> findEmployees();
 }
