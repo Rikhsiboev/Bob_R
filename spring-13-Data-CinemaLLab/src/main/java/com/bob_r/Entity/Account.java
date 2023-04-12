8eb563d4057f85cfdbf168788794b869550aa33a
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-12 17:08:29 -0600

    @OneToOne (mappedBy = "account")     private User user;

---
+++ spring-13-Data-CinemaLLab/src/main/java/com/bob_r/Entity/Account.java
@@ -0,0 +1,26 @@
+package com.bob_r.Entity;
+
+import com.bob_r.enums.UserRole;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "account_details")
+public class Account extends BaseEntity {
+    private String name;
+    private String address;
+    private String country;
+    private String city;
+    private String state;
+    private Integer age;
+    private String postalCode;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+    @OneToOne (mappedBy = "account")
+    private User user;
+
+}
---
+++ spring-13-Data-CinemaLLab/src/main/java/com/bob_r/Entity/User.java
@@ -0,0 +1,20 @@
+package com.bob_r.Entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "user_account")
+public class User extends BaseEntity{
+    private String email;
+    private String password;
+    private String username;
+    @OneToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_Details_id")
+    private Account account;
+
+}
