dd625ea1ce3417d7ddb85a37ee21f1a8ab848a98
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-12 15:39:05 -0600

    create entity with @

---
+++ spring-13-Data-CinemaLLab/src/main/java/com/bob_r/Entity/Movie.java
@@ -0,0 +1,23 @@
+package com.bob_r.Entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+
+public class Movie extends BaseEntity {
+    private String name;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "text")
+    private String summary;
+
+}
---
+++ spring-13-Data-CinemaLLab/src/main/java/com/bob_r/Spring13DataCinemaLLabApplication.java
@@ -0,0 +1,13 @@
+package com.bob_r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13DataCinemaLLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring13DataCinemaLLabApplication.class, args);
+	}
+
+}
