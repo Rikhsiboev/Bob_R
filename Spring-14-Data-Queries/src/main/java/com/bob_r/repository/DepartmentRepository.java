f8ff2a0fb84daaa718d8bd9a6993bfefae3158d8
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-13 18:58:01 -0600

    work done

--- Spring-14-Data-Queries/src/main/java/com/bob_r/repository/DepartmentRepository.java
+++ Spring-14-Data-Queries/src/main/java/com/bob_r/repository/DepartmentRepository.java
@@ -3,6 +3,7 @@ package com.bob_r.repository;
 import com.bob_r.Entity.Department;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -19,4 +20,9 @@ public interface DepartmentRepository extends JpaRepository<Department,String> {
     List<Department> findByDivisionIsEndingWith(String name);
     // Display top 3 departments with division name includes Hea without duplicates
     List<Department> findDistinctTop3ByDivisionContains(String name);
+
+    @Query("select d from Department d where d.division IN = ?1 ")
+    List<Department> retrieveDepartmentDivision(List<String> division);
+
+
 }
