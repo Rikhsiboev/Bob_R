be7ec4257391f47c2df37b8959390ad1fe54b8c6
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-18 17:45:05 -0600

    public List<RoleDTO> listAllRole() { //        List<Role> roleList = roleRepository.findAll();         return  roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());     }

--- ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/RoleServiceImpl.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/RoleServiceImpl.java
@@ -2,26 +2,29 @@ package com.Bob_R.service.impl;
 
 import com.Bob_R.dto.RoleDTO;
 import com.Bob_R.entity.Role;
+import com.Bob_R.mapper.RoleMapper;
 import com.Bob_R.repository.RoleRepository;
 import com.Bob_R.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
     public List<RoleDTO> listAllRole() {
-
-        List<Role> roleList = roleRepository.findAll();
-        return roleList;
+//        List<Role> roleList = roleRepository.findAll();
+        return  roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
