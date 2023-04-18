d7178f637ee286005c1fa275aae4239091b18461
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-17 21:47:58 -0600

    @Table(name="roles")

--- ticketing-project-mvc/src/main/java/com/Bob_R/entity/Role.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/entity/Role.java
@@ -2,13 +2,16 @@ package com.Bob_R.entity;
 
 import lombok.*;
 
+import javax.naming.Name;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name="roles")
 public class Role extends BaseEntity{
 
     private String description;
