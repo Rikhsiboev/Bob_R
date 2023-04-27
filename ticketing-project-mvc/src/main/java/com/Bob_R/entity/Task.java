804c7a2f195b49eecd5c1681bc96065cef51494b
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-26 20:38:45 -0600

    public class Task extends BaseEntity{

---
+++ ticketing-project-mvc/src/main/java/com/Bob_R/entity/Task.java
@@ -0,0 +1,40 @@
+package com.Bob_R.entity;
+
+import com.Bob_R.dto.ProjectDTO;
+import com.Bob_R.dto.UserDTO;
+import com.Bob_R.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Tasks")
+@Where(clause = "is_deleted=false")
+public class Task extends BaseEntity{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="project_id")
+    private Project project;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User assignedEmployee;
+
+
+    private String taskSubject;
+    private String taskDetail;
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignedDate;
+
+
+}
