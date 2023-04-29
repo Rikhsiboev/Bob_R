27e06600f452c623e776c0353b22ddaab9b86c1a
Rikhsiboev <rikhsiboev@gmail.com>
2023-04-28 22:42:13 -0600

    MapperUtil

--- ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/RoleServiceImpl.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/RoleServiceImpl.java
@@ -2,6 +2,7 @@ package com.Bob_R.service.impl;
 
 import com.Bob_R.dto.RoleDTO;
 import com.Bob_R.entity.Role;
+import com.Bob_R.mapper.MapperUtil;
 import com.Bob_R.mapper.RoleMapper;
 import com.Bob_R.repository.RoleRepository;
 import com.Bob_R.service.RoleService;
@@ -15,10 +16,12 @@ public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
     private final RoleMapper roleMapper;
+    private final MapperUtil mapperUtil;
 
-    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper, MapperUtil mapperUtil) {
         this.roleRepository = roleRepository;
         this.roleMapper = roleMapper;
+        this.mapperUtil = mapperUtil;
     }
 
     @Override
@@ -26,7 +29,9 @@ public class RoleServiceImpl implements RoleService {
 
         List<Role> roleList = roleRepository.findAll();
 
-        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+//        return roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+//        return roleList.stream().map(role -> mapperUtil.convert(role,new RoleDTO())).collect(Collectors.toList());
+        return roleList.stream().map(role -> mapperUtil.convert(role,RoleDTO.class)).collect(Collectors.toList());
     }
 
     @Override
