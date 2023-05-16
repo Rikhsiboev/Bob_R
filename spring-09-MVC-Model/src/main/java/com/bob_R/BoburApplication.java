9944e85a55b9eefff587f51d1c012d47a341ba35
Rikhsiboev <rikhsiboev@gmail.com>
2023-05-15 20:14:43 -0600

    fixed

--- spring-09-MVC-Model/src/main/java/com/bob_R/BoburApplication.java
+++ spring-09-MVC-Model/src/main/java/com/bob_R/BoburApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BobApplication {
+public class BoburApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BobApplication.class, args);
+		SpringApplication.run(BoburApplication.class, args);
 	}
 
 }
