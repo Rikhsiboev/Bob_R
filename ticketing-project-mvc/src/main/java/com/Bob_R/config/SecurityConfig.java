f7cb9a5745526a614a53fa8cfb09f75fd3f1e691
Rikhsiboev <rikhsiboev@gmail.com>
2023-05-17 14:47:14 -0600

    now we are creating Role 2 Authentication of form from spring we are overriding that method to use our from lognin
        @Bean
        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
            return http.authorizeRequests()// checking for role
                    .antMatchers( // any matches from http
                            "/",
                    "/login",
                    "/fragments/**",
                    "/assets/**",
                    "/images/**"
                    ).permitAll()// bring permission for user
                    .anyRequest() // any other have request
                    .authenticated() // to be authorized to be use that page or app
                    .and() // and
                    .httpBasic() // popup page Box
                    .and()//and
                    .build(); // build
    
        }

--- ticketing-project-mvc/src/main/java/com/Bob_R/config/SecurityConfig.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.Bob_R.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,5 +23,23 @@ public class SecurityConfig {
 
         userList.add(new User("Mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
         return new InMemoryUserDetailsManager(userList);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()// checking for role
+                .antMatchers( // any matches from http
+                        "/",
+                "/login",
+                "/fragments/**",
+                "/assets/**",
+                "/images/**"
+                ).permitAll()// bring permission for user
+                .anyRequest() // any other have request
+                .authenticated() // to be authorized to be use that page or app
+                .and() // and
+                .httpBasic() // popup page Box
+                .and()//and
+                .build(); // build
+
     }
 }
