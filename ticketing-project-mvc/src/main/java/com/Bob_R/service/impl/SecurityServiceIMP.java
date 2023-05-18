313d853f7179f4e118e4b7810af60fe3bae7d6c4
Rikhsiboev <rikhsiboev@gmail.com>
2023-05-17 20:42:45 -0600

    we get from Database UserRepository and throw exp new UserPrincipal(user); /// logic here get user from DB and convert  to spring object how spring understand by using UserPrincipalClass

--- ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/SecurityServiceIMP.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/service/impl/SecurityServiceIMP.java
@@ -1,6 +1,8 @@
 package com.Bob_R.service.impl;
 
+import com.Bob_R.entity.User;
 import com.Bob_R.entity.common.UserPrincipal;
+import com.Bob_R.repository.UserRepository;
 import com.Bob_R.service.SecurityServiceInterface;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,14 +10,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityServiceIMP implements SecurityServiceInterface {
-    private final UserPrincipal userPrincipal;
+    private final UserRepository userRepository;
 
-    public SecurityServiceIMP(UserPrincipal userPrincipal) {
-        this.userPrincipal = userPrincipal;
+    public SecurityServiceIMP(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null; /// logic here get user from DB and convert  to spring object how spring understand by using UserPrincipalClass
+        User user = userRepository.findByUserNameAndIsDeleted(username,false);
+
+        if (user == null){
+            throw new UsernameNotFoundException(username);
+        }
+
+
+        return new UserPrincipal(user); /// logic here get user from DB and convert  to spring object how spring understand by using UserPrincipalClass
     }
 }
