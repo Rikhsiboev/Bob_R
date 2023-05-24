d1b4ce85dba0b83e14ccfc119919f90403ee5614
Bob <rikhsiboev@gmail.com>
2023-05-24 16:12:49 -0600

    @RestController @RequestMapping("/courses/api/v2")

--- spring-15-REST/src/main/java/com/Bob_R/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/Bob_R/controller/CourseController_ResponseEntity.java
@@ -1,9 +1,22 @@
 package com.Bob_R.controller;
 
+import com.Bob_R.dto.CourseDTO;
+import com.Bob_R.service.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/courses/api/v2")
 public class CourseController_ResponseEntity {
+    private final CourseService courseService;
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
 }
