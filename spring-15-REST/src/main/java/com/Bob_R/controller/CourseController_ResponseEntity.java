5b4c08d48d6dfb1ca6d05c8718691f97e04bde03
Bob <rikhsiboev@gmail.com>
2023-05-24 16:23:58 -0600

    ResponseEntity creating status of code

--- spring-15-REST/src/main/java/com/Bob_R/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/Bob_R/controller/CourseController_ResponseEntity.java
@@ -2,15 +2,14 @@ package com.Bob_R.controller;
 
 import com.Bob_R.dto.CourseDTO;
 import com.Bob_R.service.CourseService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses/api/v2")
+@RequestMapping("/courses/api.v2")
 public class CourseController_ResponseEntity {
     private final CourseService courseService;
 
@@ -18,5 +17,31 @@ public class CourseController_ResponseEntity {
         this.courseService = courseService;
     }
 
+    @GetMapping
+    public ResponseEntity <List<CourseDTO>> getAllCourse() {
+    return ResponseEntity
+            .status(HttpStatus.ACCEPTED)
+            .header("Version","Bob_R")
+            .header("Operation","Get List")
+            .body(courseService.getCourses());
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") Long courseID){
+        return ResponseEntity.ok(courseService.getCourseById(courseID));
+
+    }
+
+    @GetMapping("category/{name}")
+    public ResponseEntity <List<CourseDTO>> getCategoryByCategory(@PathVariable("name") String category) {
+        return ResponseEntity.ok( courseService.getCoursesByCategory(category));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO courseDTO ){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation","Create")
+                .body(courseService.createCourse(courseDTO));
+    }
 
 }
