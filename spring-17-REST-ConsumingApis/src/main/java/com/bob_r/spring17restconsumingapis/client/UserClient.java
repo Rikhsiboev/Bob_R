9869f6ce4bc24440715e79132ce10571e85a2780
Bob <rikhsiboev@gmail.com>
2023-05-26 17:30:50 -0600

    by this method we can do auto from jason to our pojo but dunt forget about annotation
        @GetMapping("/user")
        List<User> getUser();

--- spring-17-REST-ConsumingApis/src/main/java/com/bob_r/spring17restconsumingapis/client/UserClient.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/bob_r/spring17restconsumingapis/client/UserClient.java
@@ -1,7 +1,15 @@
 package com.bob_r.spring17restconsumingapis.client;
 
+import com.bob_r.spring17restconsumingapis.dto.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(url = "https://jsonplaceholder.typicode.com/users",name = "User-CLIENT")
+import java.util.List;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com/users", name = "User-CLIENT")
 public interface UserClient {
+    @GetMapping("/user")
+    List<User> getUser();
+
+
 }
