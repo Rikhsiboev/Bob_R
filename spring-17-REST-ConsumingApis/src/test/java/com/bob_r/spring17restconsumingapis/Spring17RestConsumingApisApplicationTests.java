24a11ee247b576e64672e7d5aec2619ced209f17
Bob <rikhsiboev@gmail.com>
2023-05-26 17:07:37 -0600

    spring-17-REST-ConsumingApis

---
+++ spring-17-REST-ConsumingApis/src/test/java/com/bob_r/spring17restconsumingapis/Spring17RestConsumingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob_r.spring17restconsumingapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17RestConsumingApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
