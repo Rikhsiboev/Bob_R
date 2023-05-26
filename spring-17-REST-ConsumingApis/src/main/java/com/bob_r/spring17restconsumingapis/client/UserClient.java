c6c8ff8c3561c26109b839de2e60563838f54b95
Bob <rikhsiboev@gmail.com>
2023-05-26 17:28:18 -0600

    @FeignClient(url = "https://jsonplaceholder.typicode.com/users",name = "User-CLIENT")

--- spring-17-REST-ConsumingApis/src/main/java/com/bob_r/spring17restconsumingapis/client/UserClient.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/bob_r/spring17restconsumingapis/client/UserClient.java
@@ -1,4 +1,7 @@
 package com.bob_r.spring17restconsumingapis.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com/users",name = "User-CLIENT")
 public interface UserClient {
 }
