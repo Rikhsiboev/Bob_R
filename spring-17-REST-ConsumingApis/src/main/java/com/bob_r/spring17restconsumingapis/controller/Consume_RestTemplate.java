90d466e7fa20be00600ef43054c31f5a4b6ceba3
Bob <rikhsiboev@gmail.com>
2023-05-26 17:16:54 -0600

    controller with pom readAllUser Entity

---
+++ spring-17-REST-ConsumingApis/src/main/java/com/bob_r/spring17restconsumingapis/controller/Consume_RestTemplate.java
@@ -0,0 +1,26 @@
+package com.bob_r.spring17restconsumingapis.controller;
+
+import com.bob_r.spring17restconsumingapis.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/Bob")
+public class Consume_RestTemplate {
+
+    private final String URI = "https://jsonplaceholder.typicode.com/users";
+
+    private final RestTemplate restTemplate;
+
+    public Consume_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping
+    public ResponseEntity<User[]> readAllUsers(){
+
+        return restTemplate.getForEntity(URI, User[].class);
+    }}
