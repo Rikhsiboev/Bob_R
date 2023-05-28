847ede9e74dda61fd9f44c4f1dd1436140177d65
Bob <rikhsiboev@gmail.com>
2023-05-28 11:17:16 -0600

    @GetMapping("address/{id}")     public ResponseEntity<ResponseWrapper> findById(@PathVariable("id") Long addressFindById) throws Exception {         // find the addrtess to return //        AddressDTO addressDTO = addressService.findById(addressFindById);
            return ResponseEntity
                    .ok(new ResponseWrapper
                            ("Students are successfully retrieved",
                                    addressService.findById(addressFindById)));
    
        }

--- spring-18-REST-SchoolLab/src/main/java/com/Bob_r/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/Bob_r/controller/SchoolController.java
@@ -1,13 +1,16 @@
 package com.Bob_r.controller;
 
+import com.Bob_r.dto.AddressDTO;
 import com.Bob_r.dto.ResponseWrapper;
 import com.Bob_r.dto.TeacherDTO;
+import com.Bob_r.service.AddressService;
 import com.Bob_r.service.ParentService;
 import com.Bob_r.service.StudentService;
 import com.Bob_r.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,11 +20,13 @@ public class SchoolController {
     private final TeacherService teacherService;
     private final StudentService studentService;
     private final ParentService parentService;
+    private final AddressService addressService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
         this.parentService = parentService;
+        this.addressService = addressService;
     }
     @GetMapping("/teachers")
     public List<TeacherDTO> readAllTeacher() {
@@ -42,6 +47,20 @@ public class SchoolController {
                 .status(HttpStatus.ACCEPTED)
                 .header("Parent","Returned")
                 .body(responseWrapper);
+
+    }
+
+
+    @GetMapping("address/{id}")
+    public ResponseEntity<ResponseWrapper> findById(@PathVariable("id") Long addressFindById) throws Exception {
+        // find the addrtess to return
+//        AddressDTO addressDTO = addressService.findById(addressFindById);
+
+
+        return ResponseEntity
+                .ok(new ResponseWrapper
+                        ("Students are successfully retrieved",
+                                addressService.findById(addressFindById)));
 
     }
 
