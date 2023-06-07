7474539ece560eeea73ecacfa860c2af2c4644d5
Bob <rikhsiboev@gmail.com>
2023-06-07 14:12:46 -0600

    User Controller

--- ticketing-project-mvc/src/main/java/com/Bob_R/controller/UserController.java
+++ ticketing-project-mvc/src/main/java/com/Bob_R/controller/UserController.java
@@ -1,90 +1,59 @@
 package com.Bob_R.controller;
 
+import com.Bob_R.dto.ResponseWrapper;
 import com.Bob_R.dto.UserDTO;
-import com.Bob_R.service.RoleService;
 import com.Bob_R.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import java.util.List;
 
-@Controller
-@RequestMapping("/user")
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
-    private final RoleService roleService;
     private final UserService userService;
 
-    public UserController(RoleService roleService, UserService userService) {
-        this.roleService = roleService;
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/create")
-    public String createUser(Model model){
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getUsers() {
+        List<UserDTO> getAllUSer = userService.listAllUsers();
+        return ResponseEntity.ok(
+                new ResponseWrapper("Users are successfully retrieves",
+                        getAllUSer, HttpStatus.OK));
+    }
 
-        model.addAttribute("user", new UserDTO());
-        model.addAttribute("roles", roleService.listAllRoles());
-        model.addAttribute("users", userService.listAllUsers());
-
-        return "/user/create";
+    @GetMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> getUsersName(@PathVariable("username") String username) {
+        UserDTO byUserName = userService.findByUserName(username);
+        return ResponseEntity.ok(new ResponseWrapper("UserName are successfully retrieves",
+                byUserName, HttpStatus.OK));
 
     }
 
-    @PostMapping("/create")
-    public String insertUser( @Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
-
-        if (bindingResult.hasErrors()) {
-
-            model.addAttribute("roles", roleService.listAllRoles());
-            model.addAttribute("users", userService.listAllUsers());
-
-            return "/user/create";
-
-        }
-
+    @PostMapping()
+    public ResponseEntity<ResponseWrapper> createUsers(@RequestBody UserDTO user) {
         userService.save(user);
-
-        return "redirect:/user/create";
-
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new ResponseWrapper("New User created and successfully retrieves",
+                        HttpStatus.CREATED));
+    }
+    @PutMapping()
+    public ResponseEntity<ResponseWrapper> updateUsers(@RequestBody UserDTO user) {
+        UserDTO update = userService.update(user);
+        return ResponseEntity.ok(new ResponseWrapper("User update and successfully retrieves",
+                HttpStatus.OK));
+    }
+    @DeleteMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> deleteUsers(@PathVariable String username) {
+            userService.delete(username);
+            return ResponseEntity.ok(new ResponseWrapper("User deleted and successfully retrieves",
+                    HttpStatus.OK));
     }
 
-    @GetMapping("/update/{username}")
-    public String editUser( @PathVariable("username") String username, Model model) {
-
-        model.addAttribute("user", userService.findByUserName(username));
-        model.addAttribute("roles", roleService.listAllRoles());
-        model.addAttribute("users", userService.listAllUsers());
-
-        return "/user/update";
-
-    }
-
-    @PostMapping("/update")
-    public String updateUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
-
-        if (bindingResult.hasErrors()) {
-
-            model.addAttribute("roles", roleService.listAllRoles());
-            model.addAttribute("users", userService.listAllUsers());
-
-            return "/user/update";
-
-        }
-
-        userService.update(user);
-
-        return "redirect:/user/create";
-
-    }
-
-    @GetMapping("/delete/{username}")
-    public String deleteUser(@PathVariable("username") String username) {
-//        userService.deleteByUserName(username);
-        userService.delete(username);
-        return "redirect:/user/create";
-    }
 
 }
