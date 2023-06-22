c4d676447c2cb473b3d05e9612519917683fbd72
Bob <rikhsiboev@gmail.com>
2023-06-21 20:44:05 -0600

    AOP

---
+++ spring-21-AOP/src/main/java/com/bob_r/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.bob_r.repository;
+
+import com.bob_r.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByCategory(String category);
+
+}
