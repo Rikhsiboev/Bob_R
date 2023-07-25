b219b1f0d710eb8b34d7aa732db73bde50434db5
Bob <rikhsiboev@gmail.com>
2023-07-25 16:13:49 -0600

    Security app done

---
+++ ticketing-project-security/src/main/java/com/cydeo/entity/common/UserPrincipal.java
@@ -0,0 +1,67 @@
+package com.cydeo.entity.common;
+
+import com.cydeo.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+        GrantedAuthority authority = new SimpleGrantedAuthority(this.user.getRole().getDescription());
+        grantedAuthorityList.add(authority);
+
+        return grantedAuthorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassWord();//how I can access to password field of the user object
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isEnabled();
+    }
+
+    public Long getId() {  /// getID from Spring Security and reassign to our entity
+        return this.user.getId();
+    }
+
+    public String getFullName() {
+        return this.user.getFirstName() + " " + this.user.getLastName();
+    }
+
+}
