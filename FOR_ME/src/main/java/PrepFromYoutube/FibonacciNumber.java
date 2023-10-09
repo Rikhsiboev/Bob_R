61e3004b652ef819de40604c05d57fdffe43e514
Bob <rikhsiboev@gmail.com>
2023-10-09 15:22:07 -0600

    Fib

---
+++ FOR_ME/src/main/java/PrepFromYoutube/FibonacciNumber.java
@@ -0,0 +1,34 @@
+package PrepFromYoutube;
+
+import java.util.stream.Stream;
+
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+    public static int fib(int n){
+        if (n<2){
+            return n;
+        }else {
+           return fib(n-1)+fib(n-2);
+        }
+    }
+
+    public static int fibStream(int n){
+        if (n>2){
+            return n;
+        }else {
+            return Stream.iterate(
+                    new int[]{0,1},
+                    fib->new int[]{fib(1),
+                            fib[0]+fib[1]})
+                    .limit(n+1)
+                    .mapToInt(fib->fib(0))
+                    .reduce((a,b)->b)
+                    .orElse(0);
+        }
+
+    }
+
+}
