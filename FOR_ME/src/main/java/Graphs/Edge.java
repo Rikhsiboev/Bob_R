890d2b074d3d676cc62bf05b567a4f13bb54bccd
Bob <rikhsiboev@gmail.com>
2023-10-12 12:55:57 -0600

    Duplicate

---
+++ FOR_ME/src/main/java/Graphs/Edge.java
@@ -0,0 +1,11 @@
+package Graphs;
+
+public class Edge {
+    int From, to , weight;
+
+    public Edge(int from, int to, int weight) {
+        From = from;
+        this.to = to;
+        this.weight = weight;
+    }
+}
---
+++ FOR_ME/src/main/java/Graphs/GraphMainApp.java
@@ -0,0 +1,12 @@
+package Graphs;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class GraphMainApp {
+    public static void main(String[] args) {
+//        List<Edge> edgeList = Arrays.asList(new Edge(0,1,2),new Edge(0,2,4)
+//        , new Edge())
+    }
+}
